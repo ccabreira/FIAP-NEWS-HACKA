f76ada40ab02141142fe73be389b34a0a427ff49
Cíntia Cabreira <cintiacabreira@hotmail.com>
2025-02-25 23:46:22 -0300

    Corrigindo erro de Router e NewsDetail

--- site/src/App.jsx
+++ site/src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import { NewsProvider } from "./context/NewsContext";
 import BaseLayout from "./layouts/BaseLayout";
--- site/src/main.jsx
+++ site/src/main.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom"; // ✅ Importação correta
+import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext";
 import App from "./App";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <Router> {/* ✅ BrowserRouter agora está corretamente definido como Router */}
+    <BrowserRouter>
+      <AuthProvider>
         <App />
-      </Router>
-    </AuthProvider>
+      </AuthProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
+
