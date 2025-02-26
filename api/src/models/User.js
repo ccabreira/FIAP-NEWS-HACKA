a7d5a6c41d6fb8efe584129159899487177a4bf9
Cíntia Cabreira <cintiacabreira@hotmail.com>
2025-02-26 11:29:10 -0300

    Alterações BE

--- api/src/models/User.js
+++ api/src/models/User.js
@@ -1,29 +1,28 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
+const bcrypt = require("bcryptjs"); // 🔹 Certifique-se de que está usando 'bcryptjs'
 
 const UserSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  isAdmin: { type: Boolean, default: false }, // 🔹 Define o campo de admin
+  isAdmin: { type: Boolean, default: false }, // Define se o usuário é admin
 });
 
+// 🔹 Antes de salvar, criptografa a senha
 UserSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
-  this.password = await bcrypt.hash(this.password, 10);
-  next();
-});
 
-const adminUser = new User({
-  name: "Admin",
-  email: "admin@email.com",
-  password: await bcrypt.hash("123456", 10),
-  isAdmin: true
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
-await adminUser.save();
-
 
 module.exports = mongoose.model("User", UserSchema);
+
 
 
 
